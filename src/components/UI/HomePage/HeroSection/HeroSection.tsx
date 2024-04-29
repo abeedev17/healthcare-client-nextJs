9f55b176c8f5274c9a697ea952465380915f4274
abeedev17 <as.asuperstar05@gmail.com>
2024-04-29 22:18:19 +0600

    added content to hero section

--- src/components/UI/HomePage/HeroSection/HeroSection.tsx
+++ src/components/UI/HomePage/HeroSection/HeroSection.tsx
@@ -55,23 +55,106 @@ const HeroSection = () => {
           component="p"
           fontWeight={400}
           sx={{
-            width: "50%",
+            my: 4,
           }}
         >
           lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit, amet consectetur adipisicing
           elit.
         </Typography>
-        <Button>Make Appointment</Button>
-        <Button
-          variant="outlined"
+        <Box
           sx={{
-            marginLeft: "5px",
+            display: "flex",
+            gap: 2,
           }}
         >
-          Make Appointment
-        </Button>
+          <Button>Make Appointment</Button>
+          <Button
+            variant="outlined"
+            sx={{
+              marginLeft: "5px",
+            }}
+          >
+            Make Appointment
+          </Button>
+        </Box>
       </Box>
-      <Box>Right</Box>
+      <Box
+        sx={{
+          p: 1,
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          position: "relative",
+          mt: 0,
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            left: "200px",
+            top: "-30px",
+          }}
+        >
+          <Image
+            src={assets.svgs.arrow}
+            width={100}
+            height={100}
+            alt="arrow"
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Box mt={4}>
+            <Image
+              src={assets.images.doctor1}
+              width={240}
+              height={380}
+              alt="doctor1"
+            />
+          </Box>
+          <Box>
+            <Image
+              src={assets.images.doctor2}
+              width={240}
+              height={350}
+              alt="doctor2"
+            />
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "220px",
+            left: "150px",
+          }}
+        >
+          <Image
+            src={assets.images.doctor3}
+            alt="doctor3"
+            width={240}
+            height={240}
+          />
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "-50px",
+            right: "0",
+            zIndex: "-1",
+          }}
+        >
+          <Image
+            src={assets.images.stethoscope}
+            alt="doctor3"
+            width={180}
+            height={180}
+          />
+        </Box>
+      </Box>
     </Container>
   );
 };
